e252cc4970db90b0833d8b0773b617f47c3b67b3
Ryan Gildea <ryangildea@gmail.com>
2023-03-27 13:12:32 -0400

    get bundler working in browser with hard coded js

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
     const ref = useRef<any>();
@@ -23,11 +24,19 @@ const App = () => {
         if (!ref.current) {
             return;
         }
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015',
-        });
-        setCode(result.code);
+
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window'
+            }
+        })
+
+        setCode(result.outputFiles[0].text);
     };
 
     return (
