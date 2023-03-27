b7c8a810b6eb08d45f943a3b8010c46414be820c
Ryan Gildea <ryangildea@gmail.com>
2023-03-27 14:07:37 -0400

    bundler working with css files sort of

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+import localForage from "localforage";
+import * as esbuild from "esbuild-wasm";
+
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: "fetch-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: inputCode,
+          };
+        } else {
+          //   // check cache
+          //   const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          //     args.path
+          //   );
+          //   // if it is, return immediately
+          //   if (cachedResult) {
+          //     return cachedResult;
+          //   }
+
+          const { data, request } = await axios.get(args.path);
+
+          const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+          const escaped = data
+            .replace(/\n/g, "")
+            .replace(/"/g, '\\"')
+            .replace(/'/g, "\\'");
+          const contents =
+            fileType === "css"
+              ? `
+                const style = document.createElement('style');
+                style.innerText = '${escaped}';
+                document.head.appendChild(style);
+            `
+              : data;
+
+          const result: esbuild.OnLoadResult = {
+            loader: "jsx",
+            contents,
+            resolveDir: new URL("./", request.responseURL).pathname,
+          };
+          // store the response in the cache
+          await fileCache.setItem(args.path, result);
+          return result;
+        }
+      });
+    },
+  };
+};
