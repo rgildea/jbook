75b4e245bed317c38db27120e7be92c4688f023f
Ryan Gildea <ryangildea@gmail.com>
2023-03-29 11:24:37 -0400

    refactor fetch plugin

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,49 +10,58 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, async (args: any) => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
+
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
+        console.log("I ran but didn't do anything");
+        // check cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        // if it is, return immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+      });
 
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        } else {
-          //   // check cache
-          //   const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          //     args.path
-          //   );
-          //   // if it is, return immediately
-          //   if (cachedResult) {
-          //     return cachedResult;
-          //   }
-
-          const { data, request } = await axios.get(args.path);
-
-          const fileType = args.path.match(/.css$/) ? "css" : "jsx";
-          const escaped = data
-            .replace(/\n/g, "")
-            .replace(/"/g, '\\"')
-            .replace(/'/g, "\\'");
-          const contents =
-            fileType === "css"
-              ? `
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = `
                 const style = document.createElement('style');
                 style.innerText = '${escaped}';
                 document.head.appendChild(style);
-            `
-              : data;
+            `;
 
-          const result: esbuild.OnLoadResult = {
-            loader: "jsx",
-            contents,
-            resolveDir: new URL("./", request.responseURL).pathname,
-          };
-          // store the response in the cache
-          await fileCache.setItem(args.path, result);
-          return result;
-        }
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        // store the response in the cache
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        // store the response in the cache
+        await fileCache.setItem(args.path, result);
+        return result;
       });
     },
   };
