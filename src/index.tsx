5560ab4bc0b2a63b8a51caafd94c982937f59c44
Ryan Gildea <ryangildea@gmail.com>
2023-03-31 11:54:47 -0400

    remove warning

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ const App = () => {
     const ref = useRef<any>();
     const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -78,7 +77,6 @@ const App = () => {
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
             <iframe ref={iframe} title="sandbox" sandbox="allow-scripts" srcDoc={html} />
         </div >
     );
