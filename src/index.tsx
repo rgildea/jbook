d76757c445272179fd19612f7c55363b445f59d9
Ryan Gildea <ryangildea@gmail.com>
2023-04-10 14:10:16 -0400

    add basic markdown and preview card component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOMClient from 'react-dom/client';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
+
 const App = () => {
 
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
