a76fb4c6529ea42a3a06d92f0620e4915e74e38b
Ryan Gildea <ryangildea@gmail.com>
2023-04-10 12:38:05 -0400

    only bundle and execute when the user is idle for 750ms

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import Resizable from './resizable';
+import { cleanup } from '@testing-library/react';
+import bundle from '../bundler';
 
 const CodeCell = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [input]);
   // const onClick = async () => {
   //   const output = await bundle(input);
   //   setCode(output);
